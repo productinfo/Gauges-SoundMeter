6317932261609b661738f4b1d51dfcadd1a05e79
Sam Davies <sdavies@shinobicontrols.com>
2014-02-21 22:54:45 +0000

    Add some qualitative ranges to the sound meter
    Plus some simple styling

--- Gauges-SoundMeter/SCViewController.m
+++ Gauges-SoundMeter/SCViewController.m
@@ -25,11 +25,7 @@
 	// Do any additional setup after loading the view, typically from a nib.
     [ShinobiGauges setLicenseKey:@"<YOUR LICENSE KEY HERE>"];
     // Create a gauge
-    self.gauge = [[SGaugeRadial alloc] initWithFrame:CGRectInset(self.view.bounds, 40, 100)
-                                         fromMinimum:@(-60)
-                                           toMaximum:@0];
-    
-    [self.view addSubview:self.gauge];
+    [self createGauge];
     
     // Let's try the audio meter
     self.audioMeter = [[SCAudioMeter alloc] initWithSamplePeriod:0.1];
@@ -38,6 +34,36 @@
         [self.gauge setValue:10 * log10(value) duration:0.1];
     }];
     
+}
+
+#pragma mark - Utility Methods
+- (void)createGauge
+{
+    self.gauge = [[SGaugeRadial alloc] initWithFrame:CGRectInset(self.view.bounds, 40, 100)
+                                         fromMinimum:@(-60)
+                                           toMaximum:@0];
+    
+    // Set up some qualatitive ranges
+    self.gauge.qualitativeRanges = @[
+        [SGaugeQualitativeRange rangeWithMinimum:@-60
+                                         maximum:@-15
+                                           color:[[UIColor greenColor] colorWithAlphaComponent:0.4]],
+        [SGaugeQualitativeRange rangeWithMinimum:@-15
+                                         maximum:@-7.5
+                                           color:[[UIColor yellowColor] colorWithAlphaComponent:0.4]],
+        [SGaugeQualitativeRange rangeWithMinimum:@-7.5
+                                         maximum:@-2.5
+                                           color:[[UIColor orangeColor] colorWithAlphaComponent:0.4]],
+        [SGaugeQualitativeRange rangeWithMinimum:@-2.5
+                                         maximum:@0
+                                           color:[[UIColor redColor] colorWithAlphaComponent:0.4]]
+        ];
+    self.gauge.style.qualitativeRangeOuterPosition = self.gauge.style.tickBaselinePosition;
+    self.gauge.style.qualitativeRangeInnerPosition = 0.85;
+    
+    self.gauge.style.majorTickSize = CGSizeMake(2, 17);
+    
+    [self.view addSubview:self.gauge];
 }
 
 @end
